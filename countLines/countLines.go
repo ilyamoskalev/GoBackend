d97d2a298e96d5498548e29c7b9fdf4f16e4a741
ilyamoskalev <ilyamoskalev@mail.ru>
2017-08-07 20:21:57 +0300

    countLines added

---
+++ countLines/countLines.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "fmt"
+    "bytes"
+    "os"
+    "io/ioutil"
+)
+
+func countLines( fileNames []string ) {
+
+	totalSize := 0
+
+	for _, value := range fileNames{
+
+		buffer, err := ioutil.ReadFile( value )
+
+    	if err != nil {
+
+    		fmt.Println( "No such file or directory." )
+
+    	} else {
+
+    		currentSize := bytes.Count( buffer, []byte{ '\n' } )
+    		totalSize += currentSize
+    		fmt.Println( value, currentSize )
+    	
+    	}
+	}
+
+	if len( fileNames ) > 1 {
+
+		fmt.Println( "total",  totalSize )
+
+	}
+}
+
+func main() {
+
+	fileNames := os.Args[ 1: ]
+
+	if len( fileNames ) == 0 {
+
+		fmt.Println( "Enter the filename" )
+
+	} else {
+
+		countLines( fileNames )
+
+	}
+
+}
